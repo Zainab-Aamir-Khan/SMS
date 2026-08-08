98f1285e24c2044a4e44ab3df440325f3e201216
ZainabAamir <zaamir427@gmail.com>
2026-08-08 21:13:28 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,12 @@ import Schedule from './pages/student/Schedule';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Navbar />
         <Routes>
           {/* Public Routes */}
